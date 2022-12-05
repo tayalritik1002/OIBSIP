ff85d7431c2bc917056d2c234da7ae7d520640a3
tayalritik1002 <119743478+tayalritik1002@users.noreply.github.com>
2022-12-05 11:26:45 +0530

    Add files via upload

---
+++ temperature_convertor/script2.js
@@ -0,0 +1,31 @@
+const calculateTemp = () => {
+
+    const numberTemp = document.getElementById('temp').value;
+    //console.log(numberTemp);
+
+    const tempselected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempselected.selectedIndex].value;
+ 
+    const celToFah = (cel) => {
+        let fahrenheit = Math.round((cel * 9/5)  + 32);
+        return fahrenheit;
+    }
+
+    const fehToCel = (fehr) => {
+        let celsius = Math.round((fehr - 32) * 5/9 );
+        return celsius;
+    }
+    let  result;
+
+    if(valueTemp == 'cel')
+    { 
+        result = celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}°Fahrenheit`;
+    }
+    else
+    {
+        result = fehToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML= `= ${result}°Celsius`;
+    }
+
+}
